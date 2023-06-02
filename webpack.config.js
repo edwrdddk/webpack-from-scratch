1a6f260d9de335ab77c5265ec5c1f5e15ec4f79c
Edward Didyk <eduard.didyk01@gmail.com>
2023-06-02 23:09:12 +0300

    config done

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
@@ -11,7 +13,18 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
       }
-    ]
-  }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: './index.html'
+    })
+  ]
 }
